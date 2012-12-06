21cc6e8ada1e81766fe0e764edcb68f062d66eea
Tomas Corellano <hack.rapture@gmail.com>
2012-12-06 15:41:09 -0300

    Initial Commit of site app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackRapture::Application.config.secret_token = '008f49d7c39bcd25ad85042969525ffc16001eb58fee04fd1f0d7971d0502dc6a06fd9c3b626741d4a7da9ea9ba40e617f39458baa73a36b214282ffb25a4d1b'
